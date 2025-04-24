ef46ddb3e11c803b8583f787b0989bfc1bda52d3
Christophe GOUGAM <_Christophe.Gougam.ext.AZ@sodexo.com>
2025-04-24 08:58:52 +0000

    565265 - WIP manage Azure authentication Backstage

--- src/backstage/packages/app/src/App.tsx
+++ src/backstage/packages/app/src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import { Navigate, Route } from 'react-router-dom';
 import { apiDocsPlugin, ApiExplorerPage } from '@backstage/plugin-api-docs';
 import {
   CatalogEntityPage,
@@ -10,32 +8,34 @@ import {
   CatalogImportPage,
   catalogImportPlugin,
 } from '@backstage/plugin-catalog-import';
-import { ScaffolderPage, scaffolderPlugin } from '@backstage/plugin-scaffolder';
 import { orgPlugin } from '@backstage/plugin-org';
+import { ScaffolderPage, scaffolderPlugin } from '@backstage/plugin-scaffolder';
 import { SearchPage } from '@backstage/plugin-search';
 import {
   TechDocsIndexPage,
   techdocsPlugin,
   TechDocsReaderPage,
 } from '@backstage/plugin-techdocs';
-import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
+import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import { UserSettingsPage } from '@backstage/plugin-user-settings';
+import { Navigate, Route } from 'react-router-dom';
 import { apis } from './apis';
 import { entityPage } from './components/catalog/EntityPage';
-import { searchPage } from './components/search/SearchPage';
 import { Root } from './components/Root';
+import { searchPage } from './components/search/SearchPage';
 
+import { createApp } from '@backstage/app-defaults';
+import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
 import {
   AlertDisplay,
   OAuthRequestDialog,
   SignInPage,
 } from '@backstage/core-components';
-import { createApp } from '@backstage/app-defaults';
-import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
+// TODO: import { microsoftAuthApiRef } from '@backstage/core-plugin-api';
+import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
 import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
 import { RequirePermission } from '@backstage/plugin-permission-react';
-import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
 
 const app = createApp({
   apis,
@@ -58,6 +58,18 @@ const app = createApp({
   },
   components: {
     SignInPage: props => <SignInPage {...props} auto providers={['guest']} />,
+    // TODO: SignInPage: props => (
+    //   <SignInPage
+    //     {...props}
+    //     auto
+    //     provider={{
+    //       id: 'microsoft',
+    //       title: 'Microsoft',
+    //       message: 'Sign in using Azure',
+    //       apiRef: microsoftAuthApiRef,
+    //     }}
+    //   />
+    // )
   },
 });
 
